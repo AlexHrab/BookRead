0eb84067818c1cd91ab44eb32929eec1a7e720a0
AlexHrab <151546761+AlexHrab@users.noreply.github.com>
2024-06-26 16:47:17 +0300

    update26.06

--- src/Redux/Other/selectors.js
+++
@@ -1,4 +0,0 @@
-// export const selectShowContent = (state) => state.other.showContent;
-// export const selectRunDate = (state) => state.other.runDate;
-// export const selectUserStartDate = (state) => state.other.startDate;
-// export const selectUserFinishDate = (state) => state.other.finishDate;
